045e59dcef1e97d818cf4313c4797e398dd86bfa
willyanfx <willyan.fx@gmail.com>
2023-01-05 21:00:35 -0800

    13

---
+++ src/__tests__/error-boundary.js
@@ -0,0 +1,31 @@
+import {render} from '@testing-library/react'
+import * as React from 'react'
+import {ErrorBoundary} from '../error-boundary'
+import {reportError as mockReportError} from '../api'
+
+jest.mock('../api')
+
+afterEach(() => {
+  jest.clearAllMocks()
+})
+
+function Bomb({shouldThrow}) {
+  if (shouldThrow) {
+    throw new Error('💣')
+  } else {
+    return null
+  }
+}
+
+test('calls reportError and renders that there was a problem', () => {
+  mockReportError.mockResolvedValueOnce({success: true})
+  render(
+    <ErrorBoundary>
+      <Bomb shouldThrow={true} />
+    </ErrorBoundary>,
+  )
+  const error = expect.any(Error)
+  const info = {componentStack: expect.stringContaining('Bomb')}
+  expect(mockReportError).toHaveBeenCalledWith(error, info)
+  expect(mockReportError).toHaveBeenCalledTimes(1)
+})
